6ff6c1b7523e15ec264ac2cc496a88d6516cb361
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-11-05 14:58:06 +0000

    'Tensor' alpha-0.1

--- lucid/__init__.py
+++ lucid/__init__.py
@@ -2,3 +2,9 @@
 lucid
 =====
 """
+
+from lucid.main import _ArrayLike, Tensor
+
+
+def tensor(data: _ArrayLike, requires_grad: bool = False) -> Tensor:
+    return Tensor(data, requires_grad)
--- lucid/main.py
+++ lucid/main.py
@@ -43,13 +43,19 @@ class Tensor:
                 tensor.grad = None
 
     def __add__(self, other: Self) -> Self:
-        return _add(self, other)
+        return _bfunc_add(self, other)
 
     def __sub__(self, other: Self) -> Self:
-        return _sub(self, other)
+        return _bfunc_sub(self, other)
 
     def __mul__(self, other: Self) -> Self:
-        return _mul(self, other)
+        return _bfunc_mul(self, other)
+
+    def __truediv__(self, other: Self) -> Self:
+        return _bfunc_truediv(self, other)
+
+    def __pow__(self, exp: float) -> Self:
+        return _ufunc_pow(self, exp)
 
     def __repr__(self) -> str:
         return f"Tensor(data={self.data}, grad={self.grad})"
@@ -58,8 +64,16 @@ class Tensor:
         return self.data.__str__()
 
 
+def _set_tensor_grad(tensor: Tensor, grad: np.ndarray) -> None:
+    if tensor.requires_grad:
+        if tensor.grad is None:
+            tensor.grad = grad
+        else:
+            tensor.grad += grad
+
+
 # [binary] addition
-def _add(self: Tensor, other: Tensor) -> Tensor:
+def _bfunc_add(self: Tensor, other: Tensor) -> Tensor:
     other = other if isinstance(other, Tensor) else Tensor(other)
 
     result = Tensor(
@@ -67,26 +81,17 @@ def _add(self: Tensor, other: Tensor) -> Tensor:
         requires_grad=self.requires_grad or other.requires_grad,
     )
 
-    def _backward_add() -> None:
-        if self.requires_grad:
-            if self.grad is None:
-                self.grad = np.copy(result.grad)
-            else:
-                self.grad += result.grad
+    def _backward() -> None:
+        _set_tensor_grad(self, result.grad)
+        _set_tensor_grad(other, result.grad)
 
-        if other.requires_grad:
-            if other.grad is None:
-                other.grad = np.copy(result.grad)
-            else:
-                other.grad += result.grad
-
-    result._backward_op = _backward_add
+    result._backward_op = _backward
     result._prev = [self, other]
     return result
 
 
 # [binary] subtraction
-def _sub(self: Tensor, other: Tensor) -> Tensor:
+def _bfunc_sub(self: Tensor, other: Tensor) -> Tensor:
     other = other if isinstance(other, Tensor) else Tensor(other)
 
     result = Tensor(
@@ -94,26 +99,17 @@ def _sub(self: Tensor, other: Tensor) -> Tensor:
         requires_grad=self.requires_grad or other.requires_grad,
     )
 
-    def _backward_sub() -> None:
-        if self.requires_grad:
-            if self.grad is None:
-                self.grad = np.copy(result.grad)
-            else:
-                self.grad += result.grad
+    def _backward() -> None:
+        _set_tensor_grad(self, result.grad)
+        _set_tensor_grad(other, -result.grad)
 
-        if other.requires_grad:
-            if other.grad is None:
-                other.grad = -np.copy(result.grad)
-            else:
-                other.grad -= result.grad
-
-    result._backward_op = _backward_sub
+    result._backward_op = _backward
     result._prev = [self, other]
     return result
 
 
 # [binary] element-wise multiplication
-def _mul(self: Tensor, other: Tensor) -> Tensor:
+def _bfunc_mul(self: Tensor, other: Tensor) -> Tensor:
     other = other if isinstance(other, Tensor) else Tensor(other)
 
     result = Tensor(
@@ -121,28 +117,40 @@ def _mul(self: Tensor, other: Tensor) -> Tensor:
         requires_grad=self.requires_grad or other.requires_grad,
     )
 
-    def _backward_mul() -> None:
-        if self.requires_grad:
-            if self.grad is None:
-                self.grad = other.data * result.grad
-            else:
-                self.grad += other.data * result.grad
+    def _backward() -> None:
+        _set_tensor_grad(self, other.data * result.grad)
+        _set_tensor_grad(other, self.data * result.grad)
 
-        if other.requires_grad:
-            if other.grad is None:
-                other.grad = self.data * result.grad
-            else:
-                other.grad -= self.data * result.grad
-
-    result._backward_op = _backward_mul
+    result._backward_op = _backward
     result._prev = [self, other]
     return result
 
 
-a = Tensor([1, 2, 3], requires_grad=True)
-b = Tensor([4, 5, 6], requires_grad=True)
+# [binary] true-division
+def _bfunc_truediv(self: Tensor, other: Tensor) -> Tensor:
+    other = other if isinstance(other, Tensor) else Tensor(other)
 
-c = a * b + a
-c.backward()
+    result = Tensor(
+        self.data / other.data,
+        requires_grad=self.requires_grad or other.requires_grad,
+    )
 
-print(a.grad, b.grad, c.grad)
+    def _backward() -> None:
+        _set_tensor_grad(self, (1 / other.data) * result.grad)
+        _set_tensor_grad(other, -self.data / (other.data**2) * result.grad)
+
+    result._backward_op = _backward
+    result._prev = [self, other]
+    return result
+
+
+# [unary] power
+def _ufunc_pow(self: Tensor, exp: float) -> Tensor:
+    result = Tensor(self.data**exp, requires_grad=self.requires_grad)
+
+    def _backward() -> None:
+        _set_tensor_grad(self, (exp * self.data ** (exp - 1)) * result.grad)
+
+    result._backward_op = _backward
+    result._prev = [self]
+    return result
