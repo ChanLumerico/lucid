4360763e154e542fdcdadaf5b21917d9307b0ded
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-11-09 20:57:54 +0900

    Update _test.py

--- _test.py
+++ _test.py
@@ -1,11 +1,13 @@
 import lucid
-import numpy as np
 
 
-x = lucid.tensor(np.random.randn(2, 2), requires_grad=True)
-y = lucid.tensor(np.random.randn(2, 2), requires_grad=True)
+x = lucid.tensor([1.0, 3.0], requires_grad=True)
+y = lucid.tensor([2.0, 4.0], requires_grad=True)
 
-z = (x @ y + lucid.eye(2)).T
+z = x.T.dot(y) ** 2
+z = z @ x
+
+z.backward()
 
 print(lucid.diag([3, 2, 1]))
 
