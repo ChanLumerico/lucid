7454ff7b7b06a5e3e7853f2d5889dd632d84b889
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-11-10 10:21:24 +0000

    tmp-commit

--- _test.py
+++ _test.py
@@ -1,22 +1,15 @@
 import lucid
-import numpy as np
 
 
-x = lucid.tensor(np.random.randn(1), requires_grad=True)
-y = lucid.tensor(np.random.randn(1), requires_grad=True)
+x = lucid.tensor([1, 2], requires_grad=True)
+y = lucid.tensor([3, 4], requires_grad=True)
 
-learning_rate = 0.01
-num_iterations = 100
+z = x.dot(y)
 
-for iteration in range(num_iterations):
-    f = x**2 - y**2
-    f.backward()
+z.backward(keep_grad=True)
 
-    x.data -= learning_rate * x.grad
-    y.data -= learning_rate * y.grad
+print(z)
 
-    x.grad = None
-    y.grad = None
-
-    if iteration % 10 == 0:
-        print(f)
+print(z.grad)
+print(x.grad)
+print(y.grad)
