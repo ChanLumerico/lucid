d1b292ac8c39b95fdaeba6f9f680919154779cc2
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-11-10 10:21:23 +0000

    ready for nn/

--- _test.py
+++ _test.py
@@ -2,11 +2,21 @@ import lucid
 import numpy as np
 
 
-x = lucid.tensor(np.random.randn(3, 3), requires_grad=True)
+x = lucid.tensor(np.random.randn(1), requires_grad=True)
+y = lucid.tensor(np.random.randn(1), requires_grad=True)
 
-z = lucid.trace(x) * 2
+learning_rate = 0.01
+num_iterations = 100
 
-z.backward(keep_grad=True)
+for iteration in range(num_iterations):
+    f = x**2 - y**2
+    f.backward()
 
-print(z)
-print(x.grad)
+    x.data -= learning_rate * x.grad
+    y.data -= learning_rate * y.grad
+
+    x.grad = None
+    y.grad = None
+
+    if iteration % 10 == 0:
+        print(f)
