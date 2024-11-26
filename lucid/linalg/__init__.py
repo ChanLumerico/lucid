cecbccd3a95ae5460efceffeab51ed130b04078b
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-11-26 09:46:39 +0000

    renamed func.py -> _func.py

--- lucid/linalg/__init__.py
+++ lucid/linalg/__init__.py
@@ -1,42 +1,42 @@
 from lucid._tensor import Tensor
-from lucid.linalg import func
+from lucid.linalg import _func
 
 
 def inv(a: Tensor) -> Tensor:
-    return func.inv(a)
+    return _func.inv(a)
 
 
 def det(a: Tensor) -> Tensor:
-    return func.det(a)
+    return _func.det(a)
 
 
 def solve(a: Tensor, b: Tensor) -> Tensor:
-    return func.solve(a, b)
+    return _func.solve(a, b)
 
 
 def cholesky(a: Tensor) -> Tensor:
-    return func.cholesky(a)
+    return _func.cholesky(a)
 
 
 def norm(a: Tensor, ord: int = 2) -> Tensor:
-    return func.norm(a, ord)
+    return _func.norm(a, ord)
 
 
 def eig(a: Tensor) -> tuple[Tensor, Tensor]:
-    return func.eig(a)
+    return _func.eig(a)
 
 
 def qr(a: Tensor) -> tuple[Tensor, Tensor]:
-    return func.qr(a)
+    return _func.qr(a)
 
 
 def svd(a: Tensor, full_matrices: bool = True) -> tuple[Tensor, Tensor, Tensor]:
-    return func.svd(a, full_matrices)
+    return _func.svd(a, full_matrices)
 
 
 def matrix_power(a: Tensor, n: int) -> Tensor:
-    return func.matrix_power(a, n)
+    return _func.matrix_power(a, n)
 
 
 def pinv(a: Tensor) -> Tensor:
-    return func.pinv(a)
+    return _func.pinv(a)
