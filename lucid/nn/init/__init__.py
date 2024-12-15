7ac917ce15d32ecada6bf4f5dcf54007c2267309
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-12-15 00:43:50 +0000

    xavier-init wip

--- lucid/nn/init/__init__.py
+++ lucid/nn/init/__init__.py
@@ -1,9 +1,26 @@
-import lucid
+from typing import Any
 
+import lucid
 from lucid._tensor import Tensor
 from lucid.nn.init import _dist
 from lucid.types import _Scalar
 
 
-def uniform_(tensor: Tensor, a: _Scalar, b: _Scalar) -> None:
+def _tensor_check(value: Any) -> None:
+    if not isinstance(value, Tensor):
+        raise TypeError(f"Expected value to be Tensor got {type(value).__name__}.")
+
+
+def uniform_(tensor: Tensor, a: _Scalar = 0, b: _Scalar = 1) -> None:
+    _tensor_check(tensor)
     return _dist.uniform_(tensor, a, b)
+
+
+def normal_(tensor: Tensor, mean: _Scalar = 0.0, std: _Scalar = 1.0) -> None:
+    _tensor_check(tensor)
+    return _dist.normal_(tensor, mean, std)
+
+
+def constant_(tensor: Tensor, val: _Scalar) -> None:
+    _tensor_check(tensor)
+    return _dist.constant_(tensor, val)
--- lucid/nn/init/_dist.py
+++ lucid/nn/init/_dist.py
@@ -1,4 +1,4 @@
-from typing import Any
+from functools import reduce
 
 import lucid
 
@@ -6,11 +6,35 @@ from lucid._tensor import Tensor
 from lucid.types import _Scalar
 
 
-def _tensor_check(value: Any) -> None:
-    if not isinstance(value, Tensor):
-        raise TypeError(f"Expected value to be Tensor got {type(value).__name__}.")
+def _calculate_fan_in_and_fan_out(tensor: Tensor) -> tuple[int, int]:
+    if tensor.ndim == 2:
+        fan_in = tensor.shape[0]
+        fan_out = tensor.shape[1]
+
+    elif tensor.ndim in {3, 4, 5}:
+        kernel_prod = reduce(lambda x, y: x * y, tensor.shape[2:], 1)
+        fan_in = tensor.shape[1] * kernel_prod
+        fan_out = tensor.shape[0] * kernel_prod
+
+    else:
+        raise ValueError(
+            f"Tensor with dims {tensor.ndim} is not supported. "
+            + "Must be at least 2D."
+        )
+
+    return fan_in, fan_out
 
 
 def uniform_(tensor: Tensor, a: _Scalar, b: _Scalar) -> None:
-    _tensor_check(tensor)
-    ...
+    tensor.data = lucid.random.uniform(a, b, tensor.shape).data
+
+
+def normal_(tensor: Tensor, mean: _Scalar, std: _Scalar) -> None:
+    tensor.data = lucid.random.randn(tensor.shape).data * std + mean
+
+
+def constant_(tensor: Tensor, val: _Scalar) -> None:
+    tensor.data = lucid.ones_like(tensor).data * val
+
+
+def xavier_uniform_(tensor: Tensor, gain: _Scalar) -> None: ...  # TODO: Begin from here
