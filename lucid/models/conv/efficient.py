447f35beaaead4c4ede4eaafdbbe67a84e5edec8
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2025-01-16 11:07:31 +0000

    EfficientNet wip

--- lucid/models/conv/efficient.py
+++ lucid/models/conv/efficient.py
@@ -1,10 +1,23 @@
-from typing import ClassVar
+from typing import ClassVar, Type
 
 import lucid.nn as nn
 
 import lucid
 from lucid import register_model
 from lucid._tensor import Tensor
+
+
+__all__ = [
+    "EfficientNet",
+    "efficientnet_b0",
+    "efficientnet_b1",
+    "efficientnet_b2",
+    "efficientnet_b3",
+    "efficientnet_b4",
+    "efficientnet_b5",
+    "efficientnet_b6",
+    "efficientnet_b7",
+]
 
 
 class _SEBlock(nn.Module):
@@ -24,7 +37,7 @@ class _SEBlock(nn.Module):
         return x
 
 
-class MBConv(nn.Module):
+class _MBConv(nn.Module):
     expansion: ClassVar[int] = 6
 
     def __init__(
@@ -72,4 +85,141 @@ class MBConv(nn.Module):
 
     def forward(self, x: Tensor) -> Tensor:
         if self.training:
-            ...  # TODO: implement `lucid.random.bernoulli()`
+            if not lucid.random.bernoulli(self.p).item():
+                return x
+
+        x_shortcut = x
+        x_residual = self.residual(x)
+        x_se = self.se(x_residual)
+
+        x = x_se * x_residual
+        x = self.project(x)
+
+        if self.shortcut:
+            x = x_shortcut + x
+        return x
+
+
+class _SepConv(_MBConv):
+    expansion: ClassVar[int] = 1
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int = 1,
+        se_scale: int = 4,
+        p: float = 0.5,
+    ) -> None:
+        super().__init__(in_channels, out_channels, kernel_size, stride, se_scale, p)
+
+        self.residual = nn.Sequential(
+            nn.Conv2d(
+                in_channels * self.expansion,
+                in_channels * self.expansion,
+                kernel_size=kernel_size,
+                padding="same",
+                groups=in_channels * self.expansion,
+                bias=False,
+            ),
+            nn.BatchNorm2d(in_channels * self.expansion, momentum=0.99, eps=1e-3),
+            nn.Swish(),
+        )
+
+
+class EfficientNet(nn.Module):
+    def __init__(
+        self,
+        num_classes: int = 1000,
+        width_coef: float = 1.0,
+        depth_coef: float = 1.0,
+        scale: float = 1.0,
+        dropout: float = 0.2,
+        se_scale: int = 4,
+        stochastic_depth: bool = False,
+        p: float = 0.5,
+    ) -> None:
+        super().__init__()
+        channels = [32, 16, 24, 40, 80, 112, 192, 320, 1280]
+        repeats = [1, 2, 2, 3, 3, 4, 1]
+        strides = [1, 2, 2, 2, 1, 2, 1]
+        kernel_sizes = [3, 3, 5, 3, 5, 5, 3]
+
+        depth = depth_coef
+        width = width_coef
+
+        channels = [int(ch * width) for ch in channels]
+        repeats = [int(rep * depth) for rep in repeats]
+
+        if stochastic_depth:
+            self.p = p
+            self.step = 0.5 / (sum(repeats - 1))
+        else:
+            self.p = 1.0
+            self.step = 0.0
+
+        self.upsample = ...  # NOTE: need to implement `Upsample`
+
+        self.stage1 = nn.Sequential(
+            nn.Conv2d(3, channels[0], kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(channels[0], momentum=0.99, eps=1e-3),
+        )
+
+        # TODO: Make remaining stages using `_make_block()`
+
+    def _make_block(
+        self,
+        block: Type[nn.Module],
+        repeats: int,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int,
+        se_scale: int,
+    ) -> nn.Sequential:
+        strides = [stride] + [1] * (repeats - 1)
+        layers = []
+        for stride in strides:
+            layers.append(
+                block(in_channels, out_channels, kernel_size, stride, se_scale, self.p)
+            )
+            in_channels = out_channels
+            self.p -= self.step
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x: Tensor) -> Tensor:
+        NotImplemented
+
+
+@register_model
+def efficientnet_b0(num_classes: int = 1000, **kwargs) -> EfficientNet: ...
+
+
+@register_model
+def efficientnet_b1(num_classes: int = 1000, **kwargs) -> EfficientNet: ...
+
+
+@register_model
+def efficientnet_b2(num_classes: int = 1000, **kwargs) -> EfficientNet: ...
+
+
+@register_model
+def efficientnet_b3(num_classes: int = 1000, **kwargs) -> EfficientNet: ...
+
+
+@register_model
+def efficientnet_b4(num_classes: int = 1000, **kwargs) -> EfficientNet: ...
+
+
+@register_model
+def efficientnet_b5(num_classes: int = 1000, **kwargs) -> EfficientNet: ...
+
+
+@register_model
+def efficientnet_b6(num_classes: int = 1000, **kwargs) -> EfficientNet: ...
+
+
+@register_model
+def efficientnet_b7(num_classes: int = 1000, **kwargs) -> EfficientNet: ...
