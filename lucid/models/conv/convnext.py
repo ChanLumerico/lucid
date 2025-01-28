f6aa435b0706b4819ed3be3136c564120c629b94
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2025-01-28 08:22:23 +0000

    convnext_v2 family added (undoc)

--- lucid/models/conv/convnext.py
+++ lucid/models/conv/convnext.py
@@ -1,4 +1,4 @@
-from typing import override
+from typing import override, ClassVar, Type
 
 import lucid
 import lucid.nn as nn
@@ -9,11 +9,20 @@ from lucid._tensor import Tensor
 
 __all__ = [
     "ConvNeXt",
+    "ConvNeXt_V2",
     "convnext_tiny",
     "convnext_small",
     "convnext_base",
     "convnext_large",
     "convnext_xlarge",
+    "convnext_v2_atto",
+    "convnext_v2_femto",
+    "convnext_v2_pico",
+    "convnext_v2_nano",
+    "convnext_v2_tiny",
+    "convnext_v2_base",
+    "convnext_v2_large",
+    "convnext_v2_huge",
 ]
 
 
@@ -78,6 +87,8 @@ class _ChannelsFisrtLayerNorm(nn.LayerNorm):
 
 
 class ConvNeXt(nn.Module):
+    base_block: ClassVar[Type[nn.Module]] = _Block
+
     def __init__(
         self,
         num_classes: int = 1000,
@@ -108,7 +119,7 @@ class ConvNeXt(nn.Module):
         for i in range(4):
             stage = nn.Sequential(
                 *[
-                    _Block(dims[i], dp_rates[cur + j], layer_scale_init)
+                    self.base_block(dims[i], dp_rates[cur + j], layer_scale_init)
                     for j in range(depths[i])
                 ]
             )
@@ -131,6 +142,51 @@ class ConvNeXt(nn.Module):
         x = self.head(x)
 
         return x
+
+
+class _Block_V2(nn.Module):
+    def __init__(self, channels: int, drop_path: float = 0.0, *args) -> None:
+        super().__init__()
+        self.dwconv = nn.Conv2d(
+            channels, channels, kernel_size=7, padding=3, groups=channels
+        )
+        self.norm = nn.LayerNorm(channels)
+
+        self.pwconv1 = nn.Linear(channels, 4 * channels)
+        self.gelu = nn.GELU()
+        self.grn = nn.GlobalResponseNorm(4 * channels)
+
+        self.pwconv2 = nn.Linear(4 * channels, channels)
+        self.drop_path = nn.DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+
+    def forward(self, x: Tensor) -> Tensor:
+        input_ = x
+        x = self.dwconv(x).transpose((0, 2, 3, 1))
+        x = self.norm(x)
+
+        n, h, w, _ = x.shape
+        x = self.pwconv1(x.reshape(n * h * w, -1))
+        x = self.gelu(x)
+        x = self.grn(x.reshape(n, -1, h, w))
+        x = self.pwconv2(x.reshape(n * h * w, -1))
+
+        x = x.reshape(n, -1, h, w)
+        x = input_ + self.drop_path(x)
+
+        return x
+
+
+class ConvNeXt_V2(ConvNeXt):
+    base_block: ClassVar[Type[nn.Module]] = _Block_V2
+
+    def __init__(
+        self,
+        num_classes: int = 1000,
+        depths: list[int] = [3, 3, 9, 3],
+        dims: list[int] = [96, 192, 384, 768],
+        drop_path: float = 0.0,
+    ) -> None:
+        super().__init__(num_classes, depths, dims, drop_path)
 
 
 @register_model
@@ -166,3 +222,59 @@ def convnext_xlarge(num_classes: int = 1000, **kwargs) -> ConvNeXt:
     depths = [3, 3, 27, 3]
     dims = [256, 512, 1024, 2048]
     return ConvNeXt(num_classes, depths, dims, **kwargs)
+
+
+@register_model
+def convnext_v2_atto(num_classes: int = 1000, **kwargs) -> ConvNeXt_V2:
+    depths = [2, 2, 6, 2]
+    dims = [40, 80, 160, 320]
+    return ConvNeXt_V2(num_classes, depths, dims, **kwargs)
+
+
+@register_model
+def convnext_v2_femto(num_classes: int = 1000, **kwargs) -> ConvNeXt_V2:
+    depths = [2, 2, 6, 2]
+    dims = [48, 96, 192, 384]
+    return ConvNeXt_V2(num_classes, depths, dims, **kwargs)
+
+
+@register_model
+def convnext_v2_pico(num_classes: int = 1000, **kwargs) -> ConvNeXt_V2:
+    depths = [2, 2, 6, 2]
+    dims = [64, 128, 256, 512]
+    return ConvNeXt_V2(num_classes, depths, dims, **kwargs)
+
+
+@register_model
+def convnext_v2_nano(num_classes: int = 1000, **kwargs) -> ConvNeXt_V2:
+    depths = [2, 2, 8, 2]
+    dims = [80, 160, 320, 640]
+    return ConvNeXt_V2(num_classes, depths, dims, **kwargs)
+
+
+@register_model
+def convnext_v2_tiny(num_classes: int = 1000, **kwargs) -> ConvNeXt_V2:
+    depths = [3, 3, 9, 3]
+    dims = [96, 192, 384, 768]
+    return ConvNeXt_V2(num_classes, depths, dims, **kwargs)
+
+
+@register_model
+def convnext_v2_base(num_classes: int = 1000, **kwargs) -> ConvNeXt_V2:
+    depths = [3, 3, 27, 3]
+    dims = [128, 256, 512, 1024]
+    return ConvNeXt_V2(num_classes, depths, dims, **kwargs)
+
+
+@register_model
+def convnext_v2_large(num_classes: int = 1000, **kwargs) -> ConvNeXt_V2:
+    depths = [3, 3, 27, 3]
+    dims = [192, 384, 768, 1536]
+    return ConvNeXt_V2(num_classes, depths, dims, **kwargs)
+
+
+@register_model
+def convnext_v2_huge(num_classes: int = 1000, **kwargs) -> ConvNeXt_V2:
+    depths = [3, 3, 27, 3]
+    dims = [352, 704, 1408, 2816]
+    return ConvNeXt_V2(num_classes, depths, dims, **kwargs)
