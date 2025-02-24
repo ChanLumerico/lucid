be2fafdaaf0238effb48727b3598a85b2cc17020
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2025-02-24 14:47:43 +0000

    swin_v2 prep

--- lucid/models/transformer/__init__.py
+++ lucid/models/transformer/__init__.py
@@ -1,3 +1,4 @@
 from .base import *
 from .vit import *
 from .swin import *
+from .swin_v2 import *
---
+++ lucid/models/transformer/swin_v2.py
@@ -0,0 +1,10 @@
+from typing import Type
+
+import lucid
+import lucid.nn as nn
+
+from lucid import register_model
+from lucid._tensor import Tensor
+
+
+NotImplemented
