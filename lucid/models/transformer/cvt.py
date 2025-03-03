aeb89bb1c72156b9a630155abca669d2b970aa13
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2025-03-03 14:37:33 +0000

    release-1.20.11

--- lucid/models/transformer/cvt.py
+++ lucid/models/transformer/cvt.py
@@ -1,4 +1,6 @@
+from functools import partial
 from typing import Literal, Type
+from dataclasses import dataclass, field
 
 import lucid
 import lucid.nn as nn
@@ -6,6 +8,9 @@ import lucid.nn.functional as F
 
 from lucid import register_model
 from lucid._tensor import Tensor
+
+
+__all__ = ["CvT", "cvt_13", "cvt_21", "cvt_w24"]
 
 
 def _to_2tuple(val: int | float) -> tuple[int | float, ...]:
@@ -265,7 +270,233 @@ class _ConvEmbed(nn.Module):
         return x
 
 
-class _VisionTransformer(nn.Module): ...
+class _VisionTransformer(nn.Module):
+    def __init__(
+        self,
+        patch_size: int = 16,
+        patch_stride: int = 16,
+        patch_padding: int = 0,
+        in_channels: int = 3,
+        embed_dim: int = 768,
+        depth: int = 12,
+        num_heads: int = 12,
+        mlp_ratio: float = 4.0,
+        qkv_bias: bool = False,
+        drop_rate: float = 0.0,
+        attn_drop_rate: float = 0.0,
+        drop_path_rate: float = 0.0,
+        act_layer: Type[nn.Module] = nn.GELU,
+        norm_layer: Type[nn.Module] = nn.LayerNorm,
+        **kwargs,
+    ) -> None:
+        super().__init__()
+        self.num_features = self.embed_dim = embed_dim
+        self.rearrange = None
+
+        self.patch_embed = _ConvEmbed(
+            patch_size,
+            in_channels,
+            stride=patch_stride,
+            padding=patch_padding,
+            embed_dim=embed_dim,
+            norm_layer=norm_layer,
+        )
+
+        with_cls_token = kwargs["with_cls_token"]
+        if with_cls_token:
+            self.cls_token = nn.Parameter(lucid.zeros(1, 1, embed_dim))
+        else:
+            self.cls_token = None
+
+        self.pos_drop = nn.Dropout(drop_rate)
+        dpr = [x.item() for x in lucid.linspace(0, drop_path_rate, depth)]
+
+        blocks = []
+        for j in range(depth):
+            blocks.append(
+                _ConvTransformerBlock(
+                    dim_in=embed_dim,
+                    dim_out=embed_dim,
+                    num_heads=num_heads,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    drop=drop_rate,
+                    attn_drop=attn_drop_rate,
+                    drop_path=dpr[j],
+                    act_layer=act_layer,
+                    norm_layer=norm_layer,
+                    **kwargs,
+                )
+            )
+        self.blocks = nn.ModuleList(blocks)
+
+        if self.cls_token is not None:
+            nn.init.normal(self.cls_token, std=0.02)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.patch_embed(x)
+        B, _, H, W = x.shape
+
+        x = lucid.einops.rearrange(x, "b c h w -> b (h w) c")
+
+        cls_tokens = None
+        if self.cls_token is not None:
+            cls_tokens = self.cls_token.repeat(B, axis=0)
+            x = lucid.concatenate([cls_tokens, x], axis=1)
+
+        x = self.pos_drop(x)
+        for block in self.blocks:
+            x = block(x, H, W)
+
+        if self.cls_token is not None:
+            cls_tokens, x = lucid.split(x, [1, H * W], axis=1)
+
+        x = lucid.einops.rearrange(x, "b (h w) c -> b c h w", h=H, w=W)
+
+        return x, cls_tokens
 
 
-class CvT(nn.Module): ...
+@dataclass
+class CvTSpec:
+    num_stages: int
+    patch_size: list[int]
+    patch_stride: list[int]
+    patch_padding: list[int]
+    dim_embed: list[int]
+    num_heads: list[int]
+    depth: list[int]
+
+    mlp_ratio: list[float] = field(default_factory=lambda: [4.0, 4.0, 4.0])
+    attn_drop_rate: list[float] = field(default_factory=lambda: [0.0, 0.0, 0.0])
+    drop_rate: list[float] = field(default_factory=lambda: [0.0, 0.0, 0.0])
+    drop_path_rate: list[float] = field(default_factory=lambda: [0.0, 0.0, 0.1])
+
+    qkv_bias: list[bool] = field(default_factory=lambda: [True, True, True])
+    cls_token: list[bool] = field(default_factory=lambda: [False, False, True])
+    pos_embed: list[bool] = field(default_factory=lambda: [False, False, False])
+
+    qkv_proj_method: list[str] = field(
+        default_factory=lambda: ["dw_bn", "dw_bn", "dw_bn"]
+    )
+
+    kernel_qkv: list[int] = field(default_factory=lambda: [3, 3, 3])
+    padding_kv: list[int] = field(default_factory=lambda: [1, 1, 1])
+    stride_kv: list[int] = field(default_factory=lambda: [2, 2, 2])
+    padding_q: list[int] = field(default_factory=lambda: [1, 1, 1])
+    stride_q: list[int] = field(default_factory=lambda: [1, 1, 1])
+
+
+class CvT(nn.Module):
+    def __init__(
+        self,
+        in_channels: int = 3,
+        num_classes: int = 1000,
+        act_layer: Type[nn.Module] = nn.GELU,
+        norm_layer: Type[nn.Module] = nn.LayerNorm,
+        spec: CvTSpec | None = None,
+    ) -> None:
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_stages = spec.num_stages
+
+        for i in range(self.num_stages):
+            kwargs = dict(
+                patch_size=spec.patch_size[i],
+                patch_stride=spec.patch_stride[i],
+                patch_padding=spec.patch_padding[i],
+                embed_dim=spec.dim_embed[i],
+                depth=spec.depth[i],
+                num_heads=spec.num_heads[i],
+                mlp_ratio=spec.mlp_ratio[i],
+                qkv_bias=spec.qkv_bias[i],
+                drop_rate=spec.drop_rate[i],
+                attn_drop_rate=spec.attn_drop_rate[i],
+                drop_path_rate=spec.drop_path_rate[i],
+                with_cls_token=spec.cls_token[i],
+                method=spec.qkv_proj_method[i],
+                kernel_size=spec.kernel_qkv[i],
+                padding_q=spec.padding_q[i],
+                padding_kv=spec.padding_kv[i],
+                stride_kv=spec.stride_kv[i],
+                stride_q=spec.stride_q[i],
+            )
+
+            stage = _VisionTransformer(
+                in_channels=in_channels,
+                act_layer=act_layer,
+                norm_layer=norm_layer,
+                **kwargs,
+            )
+            setattr(self, f"stage{i}", stage)
+
+            in_channels = spec.dim_embed[i]
+
+        dim_embed = spec.dim_embed[-1]
+        self.norm = norm_layer(dim_embed)
+        self.cls_token = spec.cls_token[-1]
+
+        self.head = (
+            nn.Linear(dim_embed, num_classes) if num_classes > 0 else nn.Identity()
+        )
+        nn.init.normal(self.head.weight, std=0.02)
+
+    def forward(self, x: Tensor) -> Tensor:
+        for i in range(self.num_stages):
+            x, cls_tokens = getattr(self, f"stage{i}")(x)
+
+        if self.cls_token:
+            x = self.norm(cls_tokens)
+            x = lucid.squeeze(x, axis=1)
+        else:
+            x = lucid.einops.rearrange(x, "b c h w -> b (h w) c")
+            x = self.norm(x)
+            x = lucid.mean(x, axis=1)
+
+        x = self.head(x)
+        return x
+
+
+@register_model
+def cvt_13(num_classes: int = 1000, **kwargs) -> CvT:
+    spec = CvTSpec(
+        num_stages=3,
+        patch_size=[7, 3, 3],
+        patch_stride=[4, 2, 2],
+        patch_padding=[2, 1, 1],
+        dim_embed=[64, 192, 384],
+        num_heads=[1, 3, 6],
+        depth=[1, 2, 10],
+        **kwargs,
+    )
+    return CvT(
+        in_channels=3,
+        num_classes=num_classes,
+        act_layer=_QuickGELU,
+        norm_layer=partial(nn.LayerNorm, eps=1e-5),
+        spec=spec,
+    )
+
+
+@register_model
+def cvt_21(num_classes: int = 1000, **kwargs) -> CvT:
+    spec = CvTSpec(
+        num_stages=3,
+        patch_size=[7, 3, 3],
+        patch_stride=[4, 2, 2],
+        patch_padding=[2, 1, 1],
+        dim_embed=[64, 192, 384],
+        num_heads=[1, 3, 6],
+        depth=[1, 4, 16],
+        **kwargs,
+    )
+    return CvT(
+        in_channels=3,
+        num_classes=num_classes,
+        act_layer=_QuickGELU,
+        norm_layer=partial(nn.LayerNorm, eps=1e-5),
+        spec=spec,
+    )
+
+
+@register_model
+def cvt_w24(num_classes: int = 1000, **kwargs) -> CvT: ...
