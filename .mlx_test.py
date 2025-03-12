22717e145aee2efd78df8018aad0fd1c9005e33f
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2025-03-12 20:28:25 +0900

    Update .mlx_test.py

--- .mlx_test.py
+++ .mlx_test.py
@@ -3,8 +3,8 @@ import numpy as np
 import mlx.core as mx
 
 
-x = lucid.ones(2, 3, requires_grad=True).to("gpu")
-y = lucid.ones(3, 4, requires_grad=True).to("gpu")
+x = lucid.ones(200, 300, requires_grad=True).to("gpu")
+y = lucid.ones(300, 400, requires_grad=True).to("gpu")
 
 
 z = x @ y
