1ab1c10dad3547f720624068107a23e9776b575e
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2025-03-29 11:52:30 +0000

    Metal `conv2d` test

--- .mlx_test.py
+++ .mlx_test.py
@@ -6,16 +6,34 @@ import lucid.models as models
 import numpy as np
 import mlx.core as mx
 
+import time
+
 lucid.random.seed(42)
 
+device = "gpu"
 
-x = lucid.random.randn(10, 10, requires_grad=True, device="gpu")
 
-y = F.layer_norm(x, (10, 10))
-y.astype(lucid.Int16)
+x = lucid.random.randn(100, 3, 224, 224, requires_grad=True, device=device)
 
+conv = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
+conv.to(device)
+
+t0 = time.time_ns()
+
+y = conv(x)
 y.backward()
 
-print(y.dtype.__repr__())
-print(y.device)
-print(y.shape)
+t1 = time.time_ns()
+
+print(f"{(t1 - t0) / 1e6} ms")
+
+# NOTE
+# CPU: 749.361278 ms
+# GPU:   2.361677 ms
+
+# print(y.shape)
+# print(y.dtype.__repr__())
+# print(y.data.dtype)
+# print(y.device)
+
+# print(x.grad.shape)
