068be6943acd09dcf368d0e8e767e0099503fa9d
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2025-05-26 10:46:54 +0000

    RCNN `RegionWarper` prep

--- lucid/models/conv/rcnn.py
+++ lucid/models/conv/rcnn.py
@@ -5,10 +5,20 @@ import lucid.nn.functional as F
 from lucid._tensor import Tensor
 
 
-# NOTE: WIP
 class RegionWarper(nn.Module):
     def __init__(self, output_size: tuple[int, int] = (224, 224)) -> None:
         super().__init__()
         self.output_size = output_size
 
-    def forward(self, images: Tensor, rois: list[Tensor]) -> Tensor: ...
+    def forward(self, images: Tensor, rois: list[Tensor]) -> Tensor:
+        device = images.device
+        B, C, H_img, W_img = images.shape
+
+        M = sum(r.shape[0] for r in rois)
+        if M == 0:
+            return lucid.empty(0, C, *self.output_size, device=device)
+
+        boxes = lucid.concatenate(rois, axis=0).to(device)
+        img_ids = lucid.concatenate([...])  # NOTE: Implement `lucid.full`
+
+        # TODO: Should implement `F.affine_grad` and `F.grid_sample`
