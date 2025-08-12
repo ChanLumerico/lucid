c7a5a9d9189c5f35c14ef71949c8667f5923bbd6
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2025-08-12 10:58:06 +0000

    `YOLO_V2` implementation started

--- lucid/models/objdet/yolo/yolo_v2.py
+++ lucid/models/objdet/yolo/yolo_v2.py
@@ -1,1 +1,16 @@
-NotImplemented
+from lucid import register_model
+
+import lucid
+import lucid.nn as nn
+import lucid.nn.functional as F
+
+from lucid._tensor import Tensor
+from lucid.models import vggnet_16
+from lucid.models.objdet.util import iou
+
+
+__all__ = ["YOLO_V2"]
+
+
+class YOLO_V2(nn.Module):
+    NotImplemented
