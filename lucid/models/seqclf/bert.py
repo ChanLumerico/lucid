e475f166d74e8f3f70ab3f63f150ccca5ee1e05b
Chan Lee <greensox284@gmail.com>
2026-02-07 16:11:31 +0900

    BERT implementation WIP

--- lucid/models/seqclf/bert.py
+++ lucid/models/seqclf/bert.py
@@ -25,7 +25,38 @@ class _BertEmbeddings(nn.Module):
         self.dropout = nn.Dropout(hidden_dropout_prob)
 
         self.position_ids: nn.Buffer
+        self.token_type_ids: nn.Buffer
         self.register_buffer(
-            "position_ids", nn.Buffer(lucid.arange(max_position_embeddings))
+            "position_ids",
+            nn.Buffer(lucid.arange(max_position_embeddings).expand(1, -1)),
         )
-        # TODO: Implement `lucid.Tensor.expand`
+        self.register_buffer(
+            "token_type_uds",
+            nn.Buffer(lucid.zeros(*self.position_ids.shape, dtype=lucid.Long)),
+        )
+
+    def forward(
+        self,
+        input_ids: lucid.LongTensor | None = None,
+        token_type_ids: lucid.LongTensor | None = None,
+        position_ids: lucid.LongTensor | None = None,
+        inputs_embeds: lucid.FloatTensor | None = None,
+        past_key_values_length: int = 0,
+    ) -> Tensor:
+        if input_ids is not None:
+            input_shape = input_ids.shape
+        else:
+            input_shape = inputs_embeds.shape[:-1]
+
+        batch_size, seq_length = input_shape
+        if position_ids is None:
+            position_ids = self.position_ids[
+                :, past_key_values_length : seq_length + past_key_values_length
+            ]
+
+        if token_type_ids is None:
+            if hasattr(self, "token_type_ids"):
+                buffered_token_type_ids = self.token_type_ids.expand(
+                    position_ids.shape[0], -1
+                )
+                buffered_token_type_ids = ...
